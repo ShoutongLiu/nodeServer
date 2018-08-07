0e71d2c41474e09a539c3fec5d6c19c81f146d5e
liushouotng <1183063367@qq.com>
2018-08-07 18:52:49 +0800

    实现静态服务器的基本功能

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const fs = require('fs');
 const http = require('http');
 const path = require('path');
 
+//引入第三方模块包
+const mime = require('mime');
 //记录网站的根目录
-let rootPath = path.join(__dirname, 'www');
+let rootPath = path.join(__dirname, 'server');
 
 //创建服务器
 let server = http.createServer((request, response) => {
@@ -13,11 +15,41 @@ let server = http.createServer((request, response) => {
 	// 判断路径是否存在
 	if (fs.existsSync(targetPath)) {
 		//判断是文件还是文件夹
-	fs.stat(targetPath,(err,stats)=>{
+		fs.stat(targetPath, (err, stats) => {
 			if (stats.isFile()) {
-				fs.readFile(targetPath,(err,data)=>{
+				//使用mime设置类型
+				response.setHeader('content-type', mime.getType(targetPath));
+				fs.readFile(targetPath, (err, data) => {
 					//数据读取完毕
 					response.end(data);
+				});
+			}
+			//如果是文件夹就显示列表
+			if (stats.isDirectory()) {
+				fs.readdir(targetPath, (err, files) => {
+					let tem = '';
+					//遍历
+					for (let i = 0; i < files.length; i++) {
+						tem += `
+						<li>
+               				 <a href="${request.url}${request.url == '/' ? '' : '/'}${files[i]}">${files[i]}</a>
+            			</li>
+						`
+					}
+					response.end(`
+					<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+					<html>
+					<head>
+						<title>Index of/ </title>
+					</head>
+					<body>
+                  <h1>Index of ${request.url}</h1>
+                  <ul>
+                      ${tem}
+                  </ul>
+              </body>             
+              </html>
+					`);
 				});
 			}
 		});
@@ -36,6 +68,6 @@ let server = http.createServer((request, response) => {
 });
 
 //开启监听
-server.listen(8848, '127.0.0.1', () => {
+server.listen(8000, '127.0.0.1', () => {
 	console.log('开启成功');
 })
